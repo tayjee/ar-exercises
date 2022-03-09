dcf67a24a5382096c52ae4c5207cebb6209de508
Jimmy Tang <jimmy.tang.360@gmail.com>
2022-03-09 04:46:55 +0000

    Complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  @store1 = Store.find_by(id: '1')
+  puts @store1.name
+  @store2 = Store.find_by(id: '2')
+  puts @store2.name
+  @store1.name = 'Toronto'
+  puts @store1.name
+end 
